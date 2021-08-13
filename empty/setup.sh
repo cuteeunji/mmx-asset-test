f9552504eea142f35ce631e0b71c8a75c42d94ed
eunji.yeo <eunji.yeo@lge.com>
2021-08-13 12:17:06 +0900

    logger test

--- empty/setup.sh
+++ empty/setup.sh
@@ -1,2 +1,8 @@
 echo "setup.sh is called..."
 apt-get update
+
+echo "remove mmxlib"
+pip uninstall mmxlib -y
+
+echo "install mmxlib for logger test"
+pip install git+http://mod.lge.com/hub/mmx/mmx.libs.git@logger
