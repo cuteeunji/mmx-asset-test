b7085ab5ac46e6156724a62871a39050e2206895
eunji.yeo <eunji.yeo@lge.com>
2021-10-08 14:44:14 +0900

    add sleep for train

--- empty/train.py
+++ empty/train.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, time
 
 print('>>>>>> print argv')
 for arg in sys.argv:
@@ -8,4 +8,7 @@ print('>>>>>> print env variables')
 for a in os.environ:
     print(a, '=', os.getenv(a))
 
+sleep_time = os.getenv('sleep',600)
+print('>>>>>> sleep_time : {}s'.format(sleep_time))
+time.sleep(sleep_time)
 print('>>>>>> done')
