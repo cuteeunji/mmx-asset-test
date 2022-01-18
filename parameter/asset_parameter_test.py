aa6ad2855aaefdc197f7b6a0e54022e2886c8619
eunji.yeo <eunji.yeo@lge.com>
2022-01-18 17:29:06 +0900

    set train step for asset_parameter_test

--- parameter/asset_parameter_test.py
+++ parameter/asset_parameter_test.py
@@ -5,5 +5,5 @@ home = os.environ['project_home']
 workflow_history_path = os.environ['workflow_history_path']
 target_path = os.path.join(workflow_history_path, 'parameter', 'parameter.csv')
 
-df = pd.DataFrame([['asset-parameter-test', '', '', 'debug=False input_path=/data']], columns = ['workflow', 'step', 'target', 'args'])
+df = pd.DataFrame([['asset-parameter-test', 'train', '', 'debug=False input_path=/data']], columns = ['workflow', 'step', 'target', 'args'])
 df.to_csv(target_path, index= False)
